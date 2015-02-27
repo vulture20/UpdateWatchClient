110e8bdad9edf0035ba39492d29c42908f58b8f4
vulture20 <vulture20@web.de>
2015-02-27 11:23:58 +0100

    added commandline mode and update routines

--- Program.cs
+++ Program.cs
@@ -1,17 +1,113 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ServiceProcess;
+using System.Configuration.Install;
 
 namespace UpdateWatch_Client
 {
     class Program
     {
-        static void Main(string[] args)
+        public static bool console = false;
+
+        static int Main(string[] args)
         {
-            System.ServiceProcess.ServiceBase.Run(new UWClientService());
+            bool install = false, uninstall = false, rethrow = false;
+
+            try
+            {
+                foreach (string arg in args)
+                {
+                    switch (arg)
+                    {
+                        case "-i":
+                        case "-install":
+                            install = true; break;
+                        case "-u":
+                        case "-uninstall":
+                            uninstall = true; break;
+                        case "-c":
+                        case "-console":
+                            console = true; break;
+                        default:
+                            Console.Error.WriteLine("Argument not expected: " + arg);
+                            break;
+                    }
+                }
+                if (uninstall)
+                {
+                    Install(true, args);
+                }
+                if (install)
+                {
+                    Install(false, args);
+                }
+                if (console)
+                {
+                    Console.WriteLine("Starting...");
+
+                    UWClientService.initializeService();
+
+                    Console.WriteLine("System running; press any key to stop");
+                    Console.ReadKey(true);
+
+                    Console.WriteLine("System stopped");
+                }
+                else if (!(install || uninstall))
+                {
+                    rethrow = true; // so that windows sees error...
+                    System.ServiceProcess.ServiceBase.Run(new UWClientService());
+                    rethrow = false;
+                }
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                if (rethrow) throw;
+                Console.Error.WriteLine(ex.Message);
+                return -1;
+            }
+        }
+
+        static void Install(bool undo, string[] args)
+        {
+            try
+            {
+                Console.WriteLine(undo ? "uninstalling" : "installing");
+                using (AssemblyInstaller inst = new AssemblyInstaller(typeof(Program).Assembly, args))
+                {
+                    IDictionary state = new Hashtable();
+                    inst.UseNewContext = true;
+                    try
+                    {
+                        if (undo)
+                        {
+                            inst.Uninstall(state);
+                        }
+                        else
+                        {
+                            inst.Install(state);
+                            inst.Commit(state);
+                        }
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            inst.Rollback(state);
+                        }
+                        catch { }
+                        throw;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
         }
     }
 }
--- UWClientService.cs
+++ UWClientService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.ServiceProcess;
 using System.Net.Sockets;
 using System.Timers;
+using WUApiLib;
 
 namespace UpdateWatch_Client
 {
@@ -14,9 +15,11 @@ namespace UpdateWatch_Client
         //                         Std   Min  Sek  ms
         const double timerInterval = 2 * 60 * 60 * 1000;
         const Int32 timerRandom =         5 * 60 * 1000;
+        const String serverIP = "127.0.0.1";
+        const Int16 serverPort = 4584;
 
         private static System.Timers.Timer timer1;
-        Random random = new Random();
+        private static Random random = new Random();
 
         public UWClientService()
         {
@@ -26,35 +29,78 @@ namespace UpdateWatch_Client
             this.AutoLog = true;
         }
 
-        protected override void OnStart(string[] args)
+        public static void initializeService()
         {
             timer1 = new System.Timers.Timer(timerInterval + random.Next(timerRandom));
             timer1.Elapsed += new ElapsedEventHandler(OnTimer1);
             timer1.AutoReset = true;
             timer1.Enabled = true;
 //            timer1.Start();
-            
+
+            handleUpdates();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            initializeService();
+
             base.OnStart(args);
         }
 
         protected override void OnStop()
         {
+            timer1.Enabled = false;
+
             base.OnStop();
         }
 
         protected override void OnPause()
         {
+            timer1.Enabled = false;
+
             base.OnPause();
         }
 
         protected override void OnContinue()
         {
+            timer1.Enabled = true;
+
             base.OnContinue();
+        }
+
+        private static void handleUpdates()
+        {
+            UpdateSession uSession = new UpdateSession();
+            IUpdateSearcher uSearcher = uSession.CreateUpdateSearcher();
+            uSearcher.Online = false;
+            try
+            {
+                ISearchResult sResult = uSearcher.Search("IsInstalled=0 And IsHidden=0 And Type='Software'");
+                if (Program.console)
+                {
+                    Console.WriteLine("Found " + sResult.Updates.Count + " Updates:");
+                    foreach (IUpdate update in sResult.Updates)
+                    {
+                        Console.WriteLine(update.Title);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (Program.console)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                else
+                {
+                    throw;
+                }
+            }
         }
 
         private static void OnTimer1(object source, ElapsedEventArgs e)
         {
-
+            handleUpdates();
         }
     }
 }
